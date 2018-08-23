b11c01c9e3fb6b5c2edec8f541e12f80ce0b44a0
ar95314 <ar95314@gmail.com>
2018-08-23 21:39:41 +0530

    random number

---
+++ app/src/main/java/com/first/randomnumber/MainActivity.java
@@ -0,0 +1,27 @@
+package com.first.randomnumber;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+ TextView view=
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        /**
+         * Show a toast
+         * @param view -- the view that is clicked
+         */
+        public void toastMe(View view){
+            // Toast myToast = Toast.makeText(this, message, duration);
+            Toast myToast = Toast.makeText(this, "Hello Toast!",
+                    Toast.LENGTH_SHORT);
+
+
+        }
+
+}
